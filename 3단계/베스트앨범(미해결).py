694155e0be2ef502bef771fe50be2468954816b4
yongdori00 <yongdori98@gmail.com>
2022-03-15 20:03:08 +0900

    베스트앨범 미해결

---
+++ 3단계/베스트앨범(미해결).py
@@ -0,0 +1,20 @@
+def solution(genres, plays):
+    answer = []
+
+    dict_genres = {}
+
+    for i in range(len(genres)):
+        dict_genres[genres[i]] = dict_genres.get(genres[i], list()) + [(i, plays[i])]
+
+    for key, value in dict_genres.items():
+        dict_genres[key] = sorted(dict_genres[key], key = lambda x:(x[1], -x[0]), reverse = True)
+
+    dict_genres = dict(sorted(dict_genres.items(), key = lambda x:x[1][1], reverse = True))
+
+    for key, value in dict_genres.items():
+        for i in range(2):
+            answer.append(value[i][0])
+
+    return answer
+
+print(solution(["classic", "aop", "classic", "classic", "aop"], [500, 600, 150, 800, 2500]))
