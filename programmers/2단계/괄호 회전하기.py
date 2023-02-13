34379c8dbd4beaf6e5760f45df2ce3ffa8c1d47f
yongdori00 <yongdori98@gmail.com>
2023-02-14 00:52:05 +0900

    23-02/13 not exceed until O(n^2) == 1,000,000

--- programmers/2단계/괄호 회전하기.py
+++ programmers/2단계/괄호 회전하기.py
@@ -3,32 +3,64 @@ from collections import deque
 def solution(s):
     answer = 0
 
-    s_deque = deque(s)
+    sPer_deque = deque(s)
+    stack = []
 
-    for i in s:
-        print(s_deque)
-
-        pop = ""
-
-        if s_deque:
-            pop = s_deque.popleft()
-        else:
-            pop = i
-            s_deque.append(pop)
-            continue
-        
-        if s_deque:
-            if pop == "]" and s_deque[-1] == "[":
-                s_deque.pop()
-            elif pop == "}" and s_deque[-1] == "{":
-                s_deque.pop()
-            elif pop == ")" and s_deque[-1] == "(":
-                s_deque.pop()
-        else:
-            s_deque.append(pop)
-
-        if not s_deque:
+    #길이가 1~1000이라서 그냥 매번 회전 때마다 deque를 뒤에 붙이고, 해당 deque로 stack으로 괄호 새로 테스트 해줘도 됨. O(n^2) 성립 -> 1,000,000까지는 되나봄.
+    for j in range(len(s)):
+        stack = []
+        for i in sPer_deque:
+            if not stack:
+                stack.append(i)
+                continue
+            if i == "]" and stack[-1] == "[":
+                stack.pop()
+            elif i == "}" and stack[-1] == "{":
+                stack.pop()
+            elif i == ")" and stack[-1] == "(":
+                stack.pop()
+            else:
+                stack.append(i)
+        if not stack:
             answer += 1
+
+        sPer_deque.append(sPer_deque.popleft())
+
+
+    #효율성에 문제가 생길까봐 회전시키면서 바로 stack, deque가 성립하는지 체크하는 방법을 했으나 헛수고
+    # for i in s:
+    #     if not stack:
+    #         stack.append(i)
+    #         continue
+    #     if i == "]" and stack[-1] == "[":
+    #         stack.pop()
+    #     elif i == "}" and stack[-1] == "{":
+    #         stack.pop()
+    #     elif i == ")" and stack[-1] == "(":
+    #         stack.pop()
+    #     else:
+    #         stack.append(i)
+
+    # if not stack:
+    #     answer += 1
+
+    # for i in s[:-1]:
+    #     print(stack)
+        
+    #     if stack:
+    #         if i == "]" and stack[-1] == "[":
+    #             stack.pop()
+    #         elif i == "}" and stack[-1] == "{":
+    #             stack.pop()
+    #         elif i == ")" and stack[-1] == "(":
+    #             stack.pop()
+    #         else:
+    #             stack.append(i)
+    #     else:
+    #         stack.append(i)
+
+    #     if not stack:
+    #         answer += 1
 
     return answer
 
