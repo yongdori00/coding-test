7dcc594b3f8ea68662f1c37cd3a91032181872eb
JeongHyungoo <33658621+yongdori00@users.noreply.github.com>
2023-04-14 14:38:14 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.py
+++ 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.py
@@ -7,9 +7,11 @@ def solution(fees, records):
     money_records = {}
     time_records = {}
     
+    #문자열 분리
     for info in records:
         splited_records.append(info.split())
-        
+    
+    #출차를 한 경우에 문자열 시간 분리, 입/출차 시간 기록
     for io in splited_records:
         time_str = io[0].split(":")
         time = int(time_str[0]) * 60 + int(time_str[1])
@@ -20,11 +22,13 @@ def solution(fees, records):
             time_records[io[1]] = time_records.get(io[1], 0) + period
             in_records.pop(io[1])
     
+    #끝까지 출차 안한 차들
     end_time = 23 * 60 + 59
     for num, in_time in in_records.items():
         period = end_time - in_time
         time_records[num] = time_records.get(num, 0) + period
         
+    #금액 기록
     for num, in_time in time_records.items():
         if in_time <= fees[0]:
             money_records[num] = fees[1]
@@ -35,6 +39,7 @@ def solution(fees, records):
     
     # money_records = list(money_records)
     
+    #정렬
     money_records_list = sorted(money_records.items(), key = lambda x : (x[0]))
     
     for i in money_records_list:
