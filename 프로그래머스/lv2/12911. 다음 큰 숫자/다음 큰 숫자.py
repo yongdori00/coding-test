1b641369df01e259e8cd68cbccb2385ebb604437
JeongHyungoo <33658621+yongdori00@users.noreply.github.com>
2023-04-16 02:57:19 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -1,17 +1,17 @@
+#법칙을 찾으려고 하다 보니 한참 걸렸는데
+#그냥 하나씩 늘려가면서 1의 갯수만 같으면 되는거였음.
 def solution(n):
-    answer = 0
     one_cnt = 0
     big_one_cnt = 0
     
+    #초기 값의 1의 갯수
     bin_num = bin(n)
-    
     one_cnt = bin_num.count('1')
     
+    #늘려가면서 1의 갯수를 찾음
     while big_one_cnt != one_cnt:
         n += 1
         bin_num = bin(n)
         big_one_cnt = bin_num.count('1')
     
-    answer = int(bin_num, 2)
-    
-    return answer
+    return int(bin_num, 2)
