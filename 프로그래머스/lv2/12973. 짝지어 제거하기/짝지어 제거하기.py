808135b0d87a7d80af74be0ec9baa175b9d2e031
JeongHyungoo <33658621+yongdori00@users.noreply.github.com>
2023-04-18 15:18:24 +0900

    [level 2] Title: 짝지어 제거하기, Time: 121.41 ms, Memory: 14.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.py
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -1,7 +1,16 @@
 def solution(s):
     answer = -1
+    stack_list = []
+    
+    for i in range(len(s)):
+        #stack 맨 위랑 다음 문자가 같으면 pop
+        if stack_list and stack_list[-1] == s[i]:
+            stack_list.pop()
+        #아니면 append
+        else:
+            stack_list.append(s[i])
 
-    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    print('Hello Python')
-
-    return answer
+    if stack_list:
+        return 0
+    else:
+        return 1
