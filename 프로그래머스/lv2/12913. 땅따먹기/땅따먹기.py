29b1ccb5020d8913c4eb1ab5395dfca0479cbcfc
JeongHyungoo <33658621+yongdori00@users.noreply.github.com>
2023-04-20 23:45:13 +0900

    [level 2] Title: 땅따먹기, Time: 297.78 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.py
+++ 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.py
@@ -1,9 +1,10 @@
 def solution(land):
     answer = 0
 
-    #["제일 큰 값의 인덱스", "제일 큰 값", "두번째로 큰 값의 인덱스", "두번째로 큰 값"]
+    #["제일 큰 값의 인덱스", "제일 큰 값의 합", "두번째로 큰 값의 인덱스", "두번째로 큰 값의 합"]
     max_list = [[0 for i in range(4)] for i in range(len(land))]
 
+    #일단 index = 0 일때를 초기화 해줌.
     for i in range(4):
         if max_list[0][1] < land[0][i]:
             max_list[0][3] = max_list[0][1]
@@ -16,16 +17,20 @@ def solution(land):
     
     for i in range(1, len(land)):
         for j in range(4):
+            #합이 제일 큰 길의 인덱스와 다른 경우(max_list[i - 1][0]과 j의 비교 )
             if max_list[i - 1][0] != j:
+                #새로 더해서 구한 값이 기존 값보다 큰 경우 제일 큰 값으로 바꿔줌
                 if max_list[i][1] < max_list[i - 1][1] + land[i][j]:
                     max_list[i][3] = max_list[i][1]
                     max_list[i][2] = max_list[i][0]
                     max_list[i][1] = max_list[i - 1][1] + land[i][j]
                     max_list[i][0] = j
+                #두번째로 큰 값보다 큰 경우 바꿔줌.
                 elif max_list[i][3] < max_list[i - 1][1] + land[i][j]:
                     max_list[i][3] = max_list[i - 1][1] + land[i][j]
                     max_list[i][2] = j
-            else:                
+            #합이 제일 큰 길의 인덱스와 같아서 강제로 합이 두번쨰로 큰 길의 인덱스와 더해야하는 경우(max_list[i - 1][2]와 j의 배교)
+            else:
                 if max_list[i][1] < max_list[i - 1][3] + land[i][j]:
                     max_list[i][3] = max_list[i][1]
                     max_list[i][2] = max_list[i][0]
@@ -35,6 +40,7 @@ def solution(land):
                     max_list[i][3] = max_list[i - 1][3] + land[i][j]
                     max_list[i][2] = j
     
+    #마지막 인덱스에서 최대값
     answer = max_list[len(max_list) - 1][1]
     
     return answer
