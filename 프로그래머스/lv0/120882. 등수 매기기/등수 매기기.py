959d6e4830799a64f3df48fd2d8220290cc315bd
JeongHyungoo <33658621+yongdori00@users.noreply.github.com>
2023-04-20 20:43:06 +0900

    [level 0] Title: 등수 매기기, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.py
+++ 프로그래머스/lv0/120882. 등수 매기기/등수 매기기.py
@@ -6,21 +6,27 @@ def solution(score):
     num = 0
     order = 0
     
+    #인덱스, 합산 값
     for i in range(len(score)):
         order_list.append([i, score[i][0] + score[i][1]])
-        
+    
+    #합산 값으로 내림차순
     order_list.sort(reverse = True, key = lambda x : x[1])
     
     for i in range(len(order_list)):
+        #저장된 값이랑 현재가 같으면 등수 그대로
         if cur == order_list[i][1]:
             order_list[i].append(order)
+        #작아지면 현재 i값을 등수로
         else:
             order_list[i].append(i+1)
             cur = order_list[i][1]
             order = i+1
     
+    #인덱스 기준으로 오름차순
     order_list.sort(key = lambda x : x[0])
     
+    #등수만 순서대로 뽑기
     for i in order_list:
         answer.append(i[2])
     
