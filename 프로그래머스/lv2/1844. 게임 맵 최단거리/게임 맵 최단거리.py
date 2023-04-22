6d85601578cdcd7ff9abaf6a8eca3a3f546292c2
JeongHyungoo <33658621+yongdori00@users.noreply.github.com>
2023-04-23 03:52:17 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.62 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -7,29 +7,32 @@ def solution(maps):
     answer = 0
     x, y = 0, 0
     
+    #각 위치에 대한 합계
     sum_map = [[100000 for i in range(len(maps[0]))] for i in range(len(maps))]
     queue = deque()
     
+    #시작 위치에 대해서
     queue.append([x, y])
     sum_map[x][y] = 1
     
+    #queue로 풀이
     while queue:
         list_ = queue.popleft()
         x, y = list_[0], list_[1]
             
+        #상하좌우에 대해서 queue 삽입
         for i in range(4):
             ddx = x + dx[i]
             ddy = y + dy[i]
+            #map을 벗어나는 경우는 스킵
             if 0 > ddx or ddx >= len(maps[0]) or 0 > ddy or ddy >= len(maps):
                 continue
+            #벽이 아니면서 sum_map[ddy][ddx]의 값이 현재 + 1보다 크면 업데이트
             if maps[ddy][ddx] != 0 and sum_map[y][x] + 1 < sum_map[ddy][ddx]:
                 queue.append([ddx, ddy])
                 sum_map[ddy][ddx] = sum_map[y][x] + 1
-        
-        print(x, y)
     
-    print(sum_map)
-    
+    #목적지에 도달 여부
     if sum_map[len(sum_map) - 1][len(sum_map[0]) - 1] == 100000:
         answer = -1
     else:
