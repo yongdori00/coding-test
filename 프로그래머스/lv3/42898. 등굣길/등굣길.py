ba1b85c43ea3c51a3e8a8e79d04ea4b736465d80
JeongHyungoo <33658621+yongdori00@users.noreply.github.com>
2023-04-23 20:01:27 +0900

    [level 3] Title: 등굣길, Time: 5.25 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv3/42898. 등굣길/등굣길.py
+++ 프로그래머스/lv3/42898. 등굣길/등굣길.py
@@ -1,7 +1,7 @@
 from collections import deque
 
+#dfs로 하다가 시간 초과 나서 bfs로 변경
 def bfs(m, n, puddles, max_sum, visited):
-    global answer
     curm, curn = 0, 0
     
     queue = deque()
@@ -14,6 +14,9 @@ def bfs(m, n, puddles, max_sum, visited):
         if curn >= n or curm >= m:
             continue
         
+        #x == 0, y == 0과 같은 이전의 인덱스는 배열을 벗어나는 경우를 분기
+        #왼쪽, 위쪽의 max 값을 합한게 현재 위치에서 이동 갯수의 최대값이다.
+        #이걸 연속해서 나아간다.
         if curm - 1 >= 0 and curn - 1 >= 0:
             max_sum[curn][curm] = max_sum[curn - 1][curm] + max_sum[curn][curm - 1]
         elif curm - 1 < 0 and curn - 1 >= 0:
@@ -21,6 +24,8 @@ def bfs(m, n, puddles, max_sum, visited):
         elif curn - 1 < 0 and curm - 1 >= 0:
             max_sum[curn][curm] = max_sum[curn][curm - 1]
         
+        #우물이 아닌 경우에 queue에 추가해준다.
+        #visited가 중요하다 visited를 하지 않으면 시간 초과 펑!(오른쪽, 아래쪽으로만 가기 때문에 visited를 사용해주는 것이 옳은 것)
         if curm + 1 < m and curn < n and [curm + 2, curn + 1] not in puddles and not visited[curn][curm + 1]:
             queue.append([curm + 1, curn])
             visited[curn][curm + 1] = True
