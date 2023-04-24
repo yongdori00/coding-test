12180632f61857241009b6a5d6bfaf23a9e350c5
JeongHyungoo <33658621+yongdori00@users.noreply.github.com>
2023-04-24 21:55:36 +0900

    [level 3] Title: 숫자 게임, Time: 55.51 ms, Memory: 18.3 MB -BaekjoonHub

--- 프로그래머스/lv3/12987. 숫자 게임/숫자 게임.py
+++ 프로그래머스/lv3/12987. 숫자 게임/숫자 게임.py
@@ -1,12 +1,16 @@
+#최대한 격차가 작은 값들로 넘는게 최대한 많이 이길 수 있다.
 def solution(A, B):
     answer = 0
     
     indexA = 0
     indexB = 0
     
+    #오름차순으로 정렬을 해야 각 A,B의 작은 값들끼리 비교가 가능
     A.sort()
     B.sort()
     
+    #투 포인터로 B가 A보다 크면 두 인덱스를 모두 키우고
+    #A가 크거나 같으면 B의 인덱스만 값을 키운다.
     while indexA < len(A) and indexB < len(B):
         if A[indexA] < B[indexB]:
             answer += 1
