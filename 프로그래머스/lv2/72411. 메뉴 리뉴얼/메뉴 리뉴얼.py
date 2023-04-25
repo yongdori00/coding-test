dfca948a7435a4d82dc9b8bdb3dda69c983762aa
JeongHyungoo <33658621+yongdori00@users.noreply.github.com>
2023-04-25 23:43:28 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 0.52 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
+++ 프로그래머스/lv2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
@@ -47,13 +47,16 @@ def solution(orders, course):
     comb_list = []
     max_ = 0
     
+    #orders의 최대 문자열 길이가 course의 값보다 크면 해당 course 값은 pop
     for i in range(len(orders)):
         if max_ < len(orders[i]):
             max_ = len(orders[i])
-            
+    
+    
     while course[-1] > max_:
         course.pop()
             
+    #orders에 대해서 모두 combinations를 수행
     for j in orders:
         for i in course:
             if len(j) >= i:
@@ -61,16 +64,20 @@ def solution(orders, course):
     
     comb_str_list = []
     
+    #문자 리스트 -> 정렬된 문자열로 변환
     for i in comb_list:
         comb_str_list.append(''.join(sorted(i)))
     
+    #key:value를 해서 중복 값은 갯수를 찾아간다.
     for i in comb_str_list:
         dict_[i] = dict_.get(i, 0) + 1
         
+    #dict -> list(tuple(key, value))
     list_ = list()
     for key, value in dict_.items():
         list_.append((key, value))
     
+    #문자열의 길이로 오름차순 정렬
     list_.sort(key = lambda x : len(x[0]))
     
     index = 0
@@ -78,24 +85,35 @@ def solution(orders, course):
     max_str_course_index = []
     max_str_index = []
     
+    #각 문자열 길이 마다 최대 값들 뽑아내기
     for i in list_:
+        #기존 문자열 길이인 경우
         if course[index] == len(i[0]):
+            #갯수가 최대로 갱신되면 값도 갱신
             if i[1] > max_len and i[1] > 1:
                 max_len = i[1]
                 max_str_index = [i[0]]
+            #갯수가 같으면 append
             elif i[1] == max_len and i[1] > 1:
                 max_str_index.append(i[0])
                 
+        #문자열의 길이가 늘어났을 때
+        #기존 최대값 리스트 저장
+        #course의 index + 1, 최대값 초기화
         elif course[index] < len(i[0]):
             max_str_course_index += list(max_str_index)
             index += 1
             max_len = 0
+            #바로 큰 값이 나타났을 때
             if i[1] >= max_len and i[1] > 1:
                 max_len = i[1]
                 max_str_index = [i[0]]
     
+    #마지막 문자열 일단 연결
     max_str_course_index += list(max_str_index)
+    #중복 제거
     set_ = set(max_str_course_index)
+    #사전식 정렬, 리스트로 변환
     answer = sorted(set_)
     
     return answer
