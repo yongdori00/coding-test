892042046066ce95574d9d398181701fcb019b6d
JeongHyungoo <33658621+yongdori00@users.noreply.github.com>
2023-04-30 03:22:46 +0900

    [unrated] Title: 숫자 변환하기, Time: 89.40 ms, Memory: 17.4 MB -BaekjoonHub

--- 프로그래머스/unrated/154538. 숫자 변환하기/숫자 변환하기.py
+++ 프로그래머스/unrated/154538. 숫자 변환하기/숫자 변환하기.py
@@ -1,25 +1,36 @@
-from collections import deque
-
+#결국 x~y 과정을 모두 min으로 연산하는 문제임.
 def solution(x, y, n):
     answer = -1
     
+    #0~y까지 최대 순환으로 될 수 있는 값 1,000,000보다 1 큰 값으로 초기화를 함.
     list_ = [1000001 for i in range(y + 1)]
     
+    #시작 위치는 0으로 초기화
     list_[x] = 0
     
+    #x~y 순환
     for i in range(x, y + 1):
+        #거치지 않은 위치는 그냥 continue
         if list_[i] == 1000001:
             continue
+        #i + n의 기존 값 vs 현재 위치의 값 + 1 중 작은 값으로 i + n에 값 넣어줌.
         if i + n < y + 1:
             list_[i + n] = min(list_[i + n], list_[i] + 1)
+        #i * 2의 위치에 대해서 min
         if i * 2 < y + 1:
             list_[i * 2] = min(list_[i * 2], list_[i] + 1)
+        #i * 3의 위치에 대해서 min
         if i * 3 < y + 1:
             list_[i * 3] = min(list_[i * 3], list_[i] + 1)
     
+    #y위치의 값이 초기화가 된거면 answer로 출력
     if list_[y] != 1000001:
         answer = list_[y]
     return answer
+
+#queue로 풀어보려 했으나 시간 초과
+
+#from collections import deque
 #     queue = deque()
 #     queue.append([y, 0])
     
