e87439aaf0d81fb07e8f8e5a51fc03d720ab6bc0
JeongHyungoo <33658621+yongdori00@users.noreply.github.com>
2023-05-02 00:28:38 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.06 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12936. 줄 서는 방법/줄 서는 방법.py
+++ 프로그래머스/lv2/12936. 줄 서는 방법/줄 서는 방법.py
@@ -1,4 +1,5 @@
-def bfs(n, k, visited, answer, num):
+#permutation 시간 초과
+def dp(n, k, visited, answer, num):
     cnt = 0
     nk = 1
     
@@ -6,9 +7,14 @@ def bfs(n, k, visited, answer, num):
         if visited[i] == False:
             cnt += 1
             
+    #False의 갯수 - 1 만큼 factorial을 만들어줌.
+    #nk가 주기가 될겅.ㅁ
     for i in range(1, cnt):
         nk *= i
 
+    #dp
+    #k보다 작으면서 최대 값일 때 dp를 한번 더 들어감.
+    #break 안해줘도 dp로 들어가면서 visited가 전부 True로 바뀌어 있을 에정이기 때문
     for i in range(1, len(visited)):
         if visited[i] == True:
             continue
@@ -17,13 +23,14 @@ def bfs(n, k, visited, answer, num):
         else:
             visited[i] = True
             answer.append(i)
-            bfs(n, k, visited, answer, num)
+            dp(n, k, visited, answer, num)
+            break
         
 
 def solution(n, k):
     answer = []
     visited = [False for i in range(n + 1)]
     
-    bfs(n, k, visited, answer, 0)
+    dp(n, k, visited, answer, 0)
     
     return answer
