e2c16c056123b1276abadeca47495a5221a719da
JeongHyungoo <33658621+yongdori00@users.noreply.github.com>
2024-06-24 00:11:05 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/README.md
+++ 프로그래머스/2/12939. 최댓값과 최솟값/README.md
@@ -1,6 +1,6 @@
 # [level 2] 최댓값과 최솟값 - 12939 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12939?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12939) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 24일 00:08:22
+2024년 06월 24일 00:10:48
 
 ### 문제 설명
 
--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
@@ -1,8 +1,19 @@
 def solution(s):
     answer = ''
-    answerList = sorted(list(map(int, s.split())))
+    # answerList = sorted(list(map(int, s.split())))
     
-    List = [str(answerList[0]), str(answerList[len(answerList) - 1])]
+    # List = [str(answerList[0]), str(answerList[len(answerList) - 1])]
     
-    answer = ' '.join(List)
+    # answer = ' '.join(List)
+    
+    list_ = []
+    
+    s = s.split()
+    for i in s:
+        list_.append(int(i))
+        
+    list_.sort()
+    
+    answer = str(list_[0]) + ' ' + str(list_[-1])
+    
     return answer
